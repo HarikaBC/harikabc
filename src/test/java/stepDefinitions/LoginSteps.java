70088844c425a81fb9fedd63ffff4ff3265543c0
HarikaBC <harikabcteja@gmail.com>
2025-03-11 11:30:12 +0530

    Initial commit

---
+++ src/test/java/stepDefinitions/LoginSteps.java
@@ -0,0 +1,34 @@
+package stepDefinitions;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginSteps {
+@Given("I am on the login page")
+public void i_am_on_the_login_page() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+@When("I enter valid credentials")
+public void i_enter_valid_credentials() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+@When("I click on the login button")
+public void i_click_on_the_login_button() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+@Then("I should be redirected to the dashboard")
+public void i_should_be_redirected_to_the_dashboard() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+	
+}
