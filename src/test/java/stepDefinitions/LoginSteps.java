3748cb86567d50644231a25697cc7f7169f9b90c
HarikaBC <139906009+HarikaBC@users.noreply.github.com>
2025-03-11 14:08:11 +0530

    Update LoginSteps.java

--- src/test/java/stepDefinitions/LoginSteps.java
+++ src/test/java/stepDefinitions/LoginSteps.java
@@ -13,22 +13,22 @@ public class LoginSteps {
 @Given("I am on the login page")
 public void i_am_on_the_login_page() {
     // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+   // throw new io.cucumber.java.PendingException();
 }
 @When("I enter valid credentials")
 public void i_enter_valid_credentials() {
     // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+   // throw new io.cucumber.java.PendingException();
 }
 @When("I click on the login button")
 public void i_click_on_the_login_button() {
     // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+    //throw new io.cucumber.java.PendingException();
 }
 @Then("I should be redirected to the dashboard")
 public void i_should_be_redirected_to_the_dashboard() {
     // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+   // throw new io.cucumber.java.PendingException();
 }
 	
 }
